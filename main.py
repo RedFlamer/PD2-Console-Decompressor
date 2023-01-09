324ae8fffb6187635cb615fb6cf991b74087dd4f
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2023-01-09 15:15:06 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import zlib
 # byte[] chunk
 # uint32 uncompressed_chunk_size (present only if last chunk was compressed)
 
-endian = "little"
-
 for filename in os.scandir("./assets"):
     if filename.is_file() and (filename.path.find("_h.bundle") != -1 or filename.path.find("stream_") == -1 and filename.path.find("all_") == -1 and filename.path.find(".bundle") != 1): # package headers/data or all_x/stream_x headers
         print(filename.path)
 
         with open(filename.path, 'rb') as bundle, open(filename.path.replace("assets", "decompressed_assets"), 'wb') as new_bundle:
+            endian = "little"
+
             total_size_bytes = bundle.read(8) # Total size once decompressed
             total_size = int.from_bytes(total_size_bytes, endian)
             if total_size > 2**32: # Problematic if the package is >2^32 bytes once decompressed, but means big endian PS3/X360 bundles can be decompressed as well
